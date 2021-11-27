5ada1b99f5d3d4e1f8f4fbd6d4b205f1c021b2fc
Marcin Ostaszewski <m.ostaszewski@azagroup.eu>
2021-11-27 21:34:32 +0100

    add Container

---
+++ src/components/styles/Container.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+// import { theme } from './theme';
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    width: 980px;
+    max-width: 980px;
+    margin: 0 auto;
+`;

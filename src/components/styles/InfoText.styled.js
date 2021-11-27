b9c64d64a3276a41b45f61f16b01ffd0d4aaeff3
Marcin Ostaszewski <m.ostaszewski@azagroup.eu>
2021-11-27 21:32:19 +0100

    add StyledInfoText

---
+++ src/components/styles/InfoText.styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { theme } from './theme';
+
+export const StyledInfoText = styled.p`
+    color: ${theme.colors.fontActive};
+    text-transform: uppercase;
+    margin: 2px 0 4px;
+`

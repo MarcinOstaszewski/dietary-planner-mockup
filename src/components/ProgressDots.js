e0f5067d86ee1c66bb163e5c5288c9dd11da8f7c
Marcin Ostaszewski <m.ostaszewski@azagroup.eu>
2021-11-28 11:35:50 +0100

    add ProgressDots interactivity

--- src/components/ProgressDots.js
+++ src/components/ProgressDots.js
@@ -1,20 +1,23 @@
 import { StyledProgressDots } from './styles/ProgressDots.styled';
 
+import store from '../store/store';
+
 export default function ProgressDots() {
+    const createProgress = () => {
+        let dots = [];
+        for (let i = 1; i <= store.maxWeek; i++) {
+            if (i === store.currentWeek) {
+                dots.push(<li key={i} className="current"></li>);
+            } else {
+                dots.push(<li key={i}></li>);
+            }
+        }
+        return dots;
+    }
+
     return (
         <StyledProgressDots>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li className="current"></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            {createProgress()}
         </StyledProgressDots>
     )
 }

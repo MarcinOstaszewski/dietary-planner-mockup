62eb17553232e4b9cd4012b80904655ca3673b03
Marcin Ostaszewski <m.ostaszewski@azagroup.eu>
2021-11-28 10:59:57 +0100

    improve theme and Container

--- src/components/styles/Container.styled.js
+++ src/components/styles/Container.styled.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     display: flex;
-    justify-content: space-between;
     flex-direction: column;
     width: 980px;
     max-width: 980px;
--- src/components/styles/theme.js
+++ src/components/styles/theme.js
@@ -1,13 +1,14 @@
 export const theme = {
     colors: {
         borders: '#eaeaea',
-        body: '#eee',
+        darkBorders: '#e2e2e2',
         navbarBg: '#fff',
-        tableBg: '#eee',
+        darkGrey: '#eee',
         mainBg: '#f9f9f9',
         font: '#d6d6d6',
         fontActive: '#787878',
         primary: '#ff801a',
+        primaryFilter: 'invert(51%) sepia(85%) saturate(996%) hue-rotate(347deg) brightness(100%) contrast(102%)',
         secondary: '#b5c12e',
     }
 }

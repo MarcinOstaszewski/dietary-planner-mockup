be02d360d5c557bdd338a6caa3bfb2e99976cdcb
Marcin Ostaszewski <m.ostaszewski@azagroup.eu>
2021-11-29 08:39:39 +0100

    add Footer to Main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,13 +2,15 @@ import { StyledMain } from './styles/Main.styled';
 import { Container } from './styles/Container.styled';
 import Header from './Header';
 import ColumnSet from './ColumnSet';
+import Footer from './Footer';
 
 export default function Main() {
     return (
         <StyledMain>
             <Container>
                 <Header />
-                <ColumnSet></ColumnSet>
+                <ColumnSet />
+                <Footer />
             </Container>
         </StyledMain>
     );
